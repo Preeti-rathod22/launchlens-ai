a5239ac90b36f40f459e91e4dd958c43e5326fd8
Preeti-rathod22 <preetibrathod2211@gmail.com>
2026-07-10 17:01:53 +0530

    Initial project setup

---
+++ launchlens-backend/src/main/java/com/launchlens/LaunchlensBackendApplication.java
@@ -0,0 +1,13 @@
+package com.launchlens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaunchlensBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaunchlensBackendApplication.class, args);
+	}
+
+}
---
+++ launchlens-backend/src/test/java/com/launchlens/LaunchlensBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.launchlens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaunchlensBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
